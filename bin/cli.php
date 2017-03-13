124e607c3a0b8ba37af0236d16e4d2518e3875de
Luis Montealegre <montealegreluis@gmail.com>
2017-03-12 19:40:21 -0500

    Move the fixture command to the Commands namespace

--- bin/cli.php
+++ bin/cli.php
@@ -10,12 +10,12 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Helper\HelperSet;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Doctrine\DBAL\DriverManager;
-use ComPHPPuebla\Console\Command\LoadFixtureCommand;
+use ComPHPPuebla\Commands\LoadFixtureCommand;
 
 $cli = new Application('DBAL Fixtures CLI', '3.0.0');
 $cli->setCatchExceptions(true);
 
-$connection = DriverManager::getConnection(require 'config/connection.config.php');
+$connection = DriverManager::getConnection(require __DIR__ . '/../config/connection.config.php');
 
 $helperSet = new HelperSet();
 $helperSet->set(new ConnectionHelper($connection), 'db');
--- src/Commands/LoadFixtureCommand.php
+++ src/Commands/LoadFixtureCommand.php
@@ -4,7 +4,7 @@
  *
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-namespace ComPHPPuebla\Console\Command;
+namespace ComPHPPuebla\Commands;
 
 use ComPHPPuebla\Fixtures;
 use Symfony\Component\Console\Command\Command;
