5a773f3b2b19d2eede16a34bd7a6cfffa859761c
Luis Montealegre <montealegreluis@gmail.com>
2017-11-14 21:07:20 -0600

    test: Inline the setup for the RangeGenerator tests

--- tests/unit/Generators/RangeGeneratorTest.php
+++ tests/unit/Generators/RangeGeneratorTest.php
@@ -10,57 +10,16 @@ use PHPUnit\Framework\TestCase;
 
 class RangeGeneratorTest extends TestCase
 {
-    private $severalRows;
-
-    /** @test */
-    function it_ignores_rows_without_range_definitions()
-    {
-        $this->assertEquals($this->rows, $this->generator->generate($this->rows));
-    }
-
-    /** @test */
-    function it_generates_rows_for_a_single_entry_with_a_range_definition()
-    {
-        $modifiedRows = $this->generator->generate($this->singleRow);
-
-        $this->assertCount(5, $modifiedRows);
-        $this->assertArrayHasKey('station_1', $modifiedRows);
-        $this->assertArrayHasKey('station_2', $modifiedRows);
-        $this->assertArrayHasKey('station_3', $modifiedRows);
-        $this->assertArrayHasKey('station_4', $modifiedRows);
-        $this->assertArrayHasKey('station_5', $modifiedRows);
-
-        $this->assertEquals($modifiedRows['station_1'], $this->singleRow['station_[1..5]']);
-        $this->assertEquals($modifiedRows['station_2'], $this->singleRow['station_[1..5]']);
-        $this->assertEquals($modifiedRows['station_3'], $this->singleRow['station_[1..5]']);
-        $this->assertEquals($modifiedRows['station_4'], $this->singleRow['station_[1..5]']);
-        $this->assertEquals($modifiedRows['station_5'], $this->singleRow['station_[1..5]']);
-    }
-
-    /** @test */
-    function it_generates_rows_for_several_entries_with_range_definitions()
-    {
-        $modifiedRows = $this->generator->generate($this->severalRows);
-
-        $this->assertCount(5, $modifiedRows);
-        $this->assertArrayHasKey('station_1', $modifiedRows);
-        $this->assertArrayHasKey('station_2', $modifiedRows);
-        $this->assertArrayHasKey('station_3', $modifiedRows);
-        $this->assertArrayHasKey('station_4', $modifiedRows);
-        $this->assertArrayHasKey('station_5', $modifiedRows);
-
-        $this->assertEquals($modifiedRows['station_1'], $this->severalRows['station_[1..3]']);
-        $this->assertEquals($modifiedRows['station_2'], $this->severalRows['station_[1..3]']);
-        $this->assertEquals($modifiedRows['station_3'], $this->severalRows['station_[1..3]']);
-        $this->assertEquals($modifiedRows['station_4'], $this->severalRows['station_[4..5]']);
-        $this->assertEquals($modifiedRows['station_5'], $this->severalRows['station_[4..5]']);
-    }
-
     /** @before */
-    function configureGeneration()
+    function newGenerator()
     {
         $this->generator = new RangeGenerator();
-        $this->rows = [
+    }
+
+    /** @test */
+    function it_does_not_modify_rows_without_range_definitions()
+    {
+        $rowsWithoutRanges = [
             'station_1' => [
                 '`name`' => 'CASMEN GASOL',
                 '`social_reason`' => 'CASMEN SA CV',
@@ -84,7 +43,16 @@ class RangeGeneratorTest extends TestCase
                 '`last_updated_at`' => '2013-10-06 00:00:00',
             ],
         ];
-        $this->singleRow = [
+
+        $unmodifiedRows = $this->generator->generate($rowsWithoutRanges);
+
+        $this->assertEquals($rowsWithoutRanges, $unmodifiedRows);
+    }
+
+    /** @test */
+    function it_generates_rows_for_a_single_range_definition()
+    {
+        $rowWithOneRange = [
             'station_[1..5]' => [
                 '`name`' => 'CASMEN GASOL',
                 '`social_reason`' => 'CASMEN SA CV',
@@ -97,7 +65,30 @@ class RangeGeneratorTest extends TestCase
                 '`last_updated_at`' => '2013-10-06 00:00:00',
             ]
         ];
-        $this->severalRows = [
+
+        $fiveIdenticalRows = $this->generator->generate($rowWithOneRange);
+
+        $this->assertCount(5, $fiveIdenticalRows);
+
+        // It generates a unique identifier for each row
+        $this->assertArrayHasKey('station_1', $fiveIdenticalRows);
+        $this->assertArrayHasKey('station_2', $fiveIdenticalRows);
+        $this->assertArrayHasKey('station_3', $fiveIdenticalRows);
+        $this->assertArrayHasKey('station_4', $fiveIdenticalRows);
+        $this->assertArrayHasKey('station_5', $fiveIdenticalRows);
+
+        // Every row is identical
+        $this->assertEquals($fiveIdenticalRows['station_1'], $rowWithOneRange['station_[1..5]']);
+        $this->assertEquals($fiveIdenticalRows['station_2'], $rowWithOneRange['station_[1..5]']);
+        $this->assertEquals($fiveIdenticalRows['station_3'], $rowWithOneRange['station_[1..5]']);
+        $this->assertEquals($fiveIdenticalRows['station_4'], $rowWithOneRange['station_[1..5]']);
+        $this->assertEquals($fiveIdenticalRows['station_5'], $rowWithOneRange['station_[1..5]']);
+    }
+
+    /** @test */
+    function it_generates_rows_for_several_range_definitions()
+    {
+        $rowsWith2Ranges = [
             'station_[1..3]' => [
                 '`name`' => 'CASMEN GASOL',
                 '`social_reason`' => 'CASMEN SA CV',
@@ -121,14 +112,27 @@ class RangeGeneratorTest extends TestCase
                 '`last_updated_at`' => '2013-10-06 00:00:00',
             ],
         ];
+
+        $generatedRows = $this->generator->generate($rowsWith2Ranges);
+
+        $this->assertCount(5, $generatedRows);
+
+        $this->assertArrayHasKey('station_1', $generatedRows);
+        $this->assertArrayHasKey('station_2', $generatedRows);
+        $this->assertArrayHasKey('station_3', $generatedRows);
+        $this->assertArrayHasKey('station_4', $generatedRows);
+        $this->assertArrayHasKey('station_5', $generatedRows);
+
+        // It generates 3 identical rows for the first range
+        $this->assertEquals($generatedRows['station_1'], $rowsWith2Ranges['station_[1..3]']);
+        $this->assertEquals($generatedRows['station_2'], $rowsWith2Ranges['station_[1..3]']);
+        $this->assertEquals($generatedRows['station_3'], $rowsWith2Ranges['station_[1..3]']);
+
+        // It generates 2 identical rows for the second range
+        $this->assertEquals($generatedRows['station_4'], $rowsWith2Ranges['station_[4..5]']);
+        $this->assertEquals($generatedRows['station_5'], $rowsWith2Ranges['station_[4..5]']);
     }
 
     /** @var RangeGenerator */
     private $generator;
-
-    /** @var array */
-    private $rows;
-
-    /** @var array */
-    private $singleRow;
 }
